77e0809b0d3e1a72863d38c8698115f4ce31f8f6
Tim Burris <tim.burris@ectobox.com>
2018-07-05 13:58:18 -0400

    added a unity adapter

---
+++ src/NinjaMvvm/NinjaMvvm.Wpf.Unity/Component.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Unity;
+using Unity.Injection;
+
+namespace NinjaMvvm.Wpf.Unity
+{
+    public static class Component
+    {
+        public static void Init<TNavigator>(IUnityContainer container)
+            where TNavigator : Abstractions.INavigator
+        {
+            container.RegisterInstance<IUnityContainer>(container);
+            container.RegisterType<Abstractions.IViewModelResolver, ViewModelResolver>();
+
+            container.RegisterType<Abstractions.INavigator, TNavigator>();
+
+        }
+        public static IUnityContainer Init<TNavigator>()
+            where TNavigator : Abstractions.INavigator
+        {
+            var container = new UnityContainer();
+            Init<TNavigator>(container);
+            return container;
+        }
+    }
+}
---
+++ src/NinjaMvvm/NinjaMvvm.Wpf.Unity/ViewModelResolver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace NinjaMvvm.Wpf.Unity
+{
+    public class ViewModelResolver : Abstractions.IViewModelResolver
+    {
+        private readonly IUnityContainer _container;
+
+        public ViewModelResolver(IUnityContainer container)
+        {
+            this._container = container;
+        }
+        public TViewModel Resolve<TViewModel>()
+        {
+            return _container.Resolve<TViewModel>();
+        }
+    }
+}
