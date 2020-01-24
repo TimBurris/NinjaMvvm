55e0b9cd9f01c4b022c15892850edbbdee83b1f4
Tim Burris <tim.burris@chalklogic.com>
2020-01-24 06:02:08 -0500

    added constructors for paramless relays

--- src/NinjaMvvm/NinjaMvvm.Wpf/RelayCommand.cs
+++ src/NinjaMvvm/NinjaMvvm.Wpf/RelayCommand.cs
@@ -43,10 +43,22 @@ namespace NinjaMvvm.Wpf
     /// </summary>
     public class RelayCommand : RelayCommand<object>
     {
+        public RelayCommand(Action execute) : base(new Action<object>((param) => execute())) { }
+        public RelayCommand(Action execute, Func<bool> canExecute) : base(new Action<object>((param) => execute()), new Predicate<object>((param) => canExecute())) { }
         public RelayCommand(Action<object> execute) : base(execute) { }
 
         public RelayCommand(Action<object> execute, Predicate<object> canExecute) : base(execute, canExecute) { }
 
         public RelayCommand(Action<object> execute, Predicate<object> canExecute, string label) : base(execute, canExecute, label) { }
+
+        public void Execute()
+        {
+            base.Execute(parameter: null);
+        }
+
+        public bool CanExecute()
+        {
+            return base.CanExecute(parameter: null);
+        }
     }
 }
