99c0e6b14550928fc0cc39a53726f8fe0462891e
Tim Burris <tim.burris@chalklogic.com>
2020-03-02 08:17:22 -0500

    added an overload of GetAndBindPage that will handle resolving the pagemodel, rather than requiring it be passed in

--- src/NinjaMvvm/NinjaMvvm.Xam/Abstractions/IPageResolver.cs
+++ src/NinjaMvvm/NinjaMvvm.Xam/Abstractions/IPageResolver.cs
@@ -1,8 +1,11 @@
-﻿namespace NinjaMvvm.Xam.Abstractions
+﻿using System;
+
+namespace NinjaMvvm.Xam.Abstractions
 {
     public interface IPageResolver
     {
         Xamarin.Forms.Page GetAndBindPage<TPageModel>(TPageModel pageModel) where TPageModel : class, IPageModel;
+        Xamarin.Forms.Page GetAndBindPage<TPageModel>(Action<TPageModel> initAction = null) where TPageModel : class, IPageModel;
     }
 
 }
--- src/NinjaMvvm/NinjaMvvm.Xam/XamvvmPageResolver.cs
+++ src/NinjaMvvm/NinjaMvvm.Xam/XamvvmPageResolver.cs
@@ -8,9 +8,24 @@ namespace NinjaMvvm.Xam
 {
     public class XamvvmPageResolver : Abstractions.IPageResolver
     {
+        private readonly IPageModelFactory _pageModelFactory;
+
+        public XamvvmPageResolver(IPageModelFactory pageModelFactory)
+        {
+            _pageModelFactory = pageModelFactory;
+        }
         public Xamarin.Forms.Page GetAndBindPage<TPageModel>(TPageModel pageModel) where TPageModel : class, IPageModel
         {
             return Xamvvm.XamvvmCore.CurrentFactory.GetPageAsNewInstance(pageModel) as Xamarin.Forms.Page;
+        }
+
+        public Xamarin.Forms.Page GetAndBindPage<TPageModel>(Action<TPageModel> initAction = null) where TPageModel : class, IPageModel
+        {
+            var pageModel = _pageModelFactory.GetPageModel<TPageModel>();
+
+            initAction?.Invoke(pageModel);
+
+            return this.GetAndBindPage(pageModel);
         }
     }
 }
