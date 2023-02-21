c511748a871304b88f852eb2d613de18504e0fe4
Tim Burris <tim.burris@chalklogic.com>
2023-02-21 12:52:42 -0500

    fixed typo in namespace

--- src/NinjaMvvm/NinjaMvvm.Wpf/Contols/BindableContentPresenter.cs
+++ src/NinjaMvvm/NinjaMvvm.Wpf/Contols/BindableContentPresenter.cs
@@ -1,39 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NinjaMvvm.Wpf.Contols
+﻿namespace NinjaMvvm.Wpf.Controls
 {
-	public class BindableContentPresenter : System.Windows.Controls.ContentPresenter
-	{
-		public BindableContentPresenter()
-		{
-			this.DataContextChanged += BindableContentPresenter_DataContextChanged;
-			this.Unloaded += BindableContentPresenter_Unloaded;
-		}
+    public class BindableContentPresenter : System.Windows.Controls.ContentPresenter
+    {
+        public BindableContentPresenter()
+        {
+            this.DataContextChanged += BindableContentPresenter_DataContextChanged;
+            this.Unloaded += BindableContentPresenter_Unloaded;
+        }
 
-		private void BindableContentPresenter_Unloaded(object sender, System.Windows.RoutedEventArgs e)
-		{
-			(this.DataContext as NinjaMvvm.Wpf.WpfViewModelBase)?.ViewHasBeenUnbound();
-			this.DataContextChanged -= BindableContentPresenter_DataContextChanged;
-			this.Unloaded -= BindableContentPresenter_Unloaded;
-		}
+        private void BindableContentPresenter_Unloaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            (this.DataContext as NinjaMvvm.Wpf.WpfViewModelBase)?.ViewHasBeenUnbound();
+            this.DataContextChanged -= BindableContentPresenter_DataContextChanged;
+            this.Unloaded -= BindableContentPresenter_Unloaded;
+        }
 
-		private void BindableContentPresenter_DataContextChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
-		{
-			(e.OldValue as NinjaMvvm.Wpf.WpfViewModelBase)?.ViewHasBeenUnbound();
+        private void BindableContentPresenter_DataContextChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
+        {
+            (e.OldValue as NinjaMvvm.Wpf.WpfViewModelBase)?.ViewHasBeenUnbound();
 
-			var vm = e.NewValue as NinjaMvvm.Wpf.WpfViewModelBase;
+            var vm = e.NewValue as NinjaMvvm.Wpf.WpfViewModelBase;
 
-			if (vm == null)
-				return;
-			else
-			{
-				//tells the viewmodel it has been bound
-				object value = vm.ViewBound;
-			}
-		}
-	}
+            if (vm == null)
+                return;
+            else
+            {
+                //tells the viewmodel it has been bound
+                object value = vm.ViewBound;
+            }
+        }
+    }
 }
